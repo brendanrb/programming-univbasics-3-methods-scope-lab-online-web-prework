051f9d0ca2dd0f1f6a40de461bdfd4444d51565a
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-14 13:43:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,12 @@
 def mario
-  phrase = "It's-a me, Mario!"
-  puts out #{phrase}
+  phrase = "'It's-a me, Mario!'"
+  puts #{phrase}
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts out #{status}
+  phrase = "'Thank You Mario! But Our Princess Is In Another Castle!'"
+  puts #{phrase}
 end
+
+def link
+  phrase "'It's Dangerous To Go Alone! Take This.''
