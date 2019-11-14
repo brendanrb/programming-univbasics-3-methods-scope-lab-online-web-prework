37e68dcfc3cf6244aea41bfe06f607c71ac61b11
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-14 13:39:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,6 @@ def mario
 end
 
 def toadstool
-  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts #{phrase}
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts #{status}
 end
